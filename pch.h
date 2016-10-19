c7613934956d010e9ad8fd99be125cf71a1b9d99
highfence <highfence@naver.com>
2016-10-19 11:45:03 +0900

    Pragma Warning 처리가 된 프리컴파일드 헤더

---
+++ pch.h
@@ -0,0 +1,9 @@
+
+#pragma warning(push)
+#pragma warning(disable : 4100 4245 4819)
+#include "gainput/gainput.h"
+#pragma comment(lib, "gainputstatic.lib")
+#pragma comment(lib, "xinput.lib")
+#include "cocos2d.h"
+#pragma warning(pop)
+USING_NS_CC;
